f424472c2ed7d9a1946662814eb741bd3fdbf80e
banusion <banusion69@free.fr>
2017-06-23 22:18:38 +0200

    Google ID

--- config/auth.js
+++ config/auth.js
@@ -5,16 +5,9 @@ module.exports = {
         'clientSecret': '8c7e962b07194ad9ebe4d932351e58a2', // your App Secret
         'callbackURL': 'http://localhost:8069/auth/facebook/callback'
     },
-
-    'twitterAuth': {
-        'consumerKey': 'your-consumer-key-here',
-        'consumerSecret': 'your-client-secret-here',
-        'callbackURL': 'http://localhost:8069/auth/twitter/callback'
-    },
-
     'googleAuth': {
-        'clientID': 'your-secret-clientID-here',
-        'clientSecret': 'your-client-secret-here',
+        'clientID': '994428787075-cfbtcsb6bheujg6r7uf5elikgeopvcb0.apps.googleusercontent.com',
+        'clientSecret': 'RahDjSg7lUb3PFnIBWrFRpFH',
         'callbackURL': 'http://localhost:8069/auth/google/callback'
     }
 }
